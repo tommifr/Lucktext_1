a5352f390b990a79c989ceab6cb07884a328cea1
Tommi Frykman <Tommi.Frykman@elev.ga.ntig.se>
2022-08-30 11:00:20 +0200

    första commit

---
+++ Lucktext_1/Program.cs
@@ -0,0 +1,34 @@
+﻿string name;
+string mat;
+string skola;
+
+Console.WriteLine("helo u naeme is?");
+ 
+name = Console.ReadLine();
+
+Console.WriteLine("vad gillar du för mat?");
+mat = Console.ReadLine();
+
+Console.WriteLine("Vilken skola du gå på?");
+
+skola= Console.ReadLine();
+
+
+
+Console.WriteLine($"hello {name}");
+
+
+
+Console.WriteLine("välkomna");
+
+
+
+Console.WriteLine($"so {name} u like {mat} pretty bad if u ask me ");
+Console.WriteLine("cringe");
+Console.WriteLine($"så cringe att hela {skola} tycker du är cringe");
+Console.WriteLine("tryck enter för avsluta");
+Console.ReadLine();
+
+
+
+
